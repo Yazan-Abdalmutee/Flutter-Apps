ed0a6edd98b4daea7160bc563dd9ef416ba91e2a
Yazan-Almutte-CE <yazan.abd.almutte@gmail.com>
2025-04-19 12:05:08 +0300

    initial commit

--- lib/fitness_magazine/views/selected_card.dart
+++ lib/fitness_magazine/views/selected_card.dart
@@ -22,65 +22,78 @@ class SelectedCard extends StatelessWidget {
       supportedLocales: appSupportedLocales,
 
       home: Scaffold(
-        body: Column(
-          children: [
-            Stack(
-              children: [
-                Hero(
-                   tag: mainCardData.tag,
-                  child: SizedBox(
-                    height: 300,
-                    child: CachedNetworkImage(
-                      imageUrl: mainCardData.imageAssetPath,
-                      fit:BoxFit.fill ,
+        body: SizedBox(
+          height: double.infinity,
+          child: Stack(
+            children: [
+              Stack(
+                children: [
+                  Hero(
+                    tag: mainCardData.tag,
+                    child: SizedBox(
+                      height: 300,
+                      child: CachedNetworkImage(
+                        imageUrl: mainCardData.imageAssetPath,
+                        fit: BoxFit.fill,
+                      ),
                     ),
                   ),
-                ),
-                Positioned(
-                  top: 13,
-                  right: 8,
-                  child: IconButton(
-                    onPressed: () {
-                      Navigator.pop(context);
-                    },
-                    icon: Icon(Icons.arrow_back, color: Colors.white),
+                  Positioned(
+                    top: 13,
+                    right: 8,
+                    child: IconButton(
+                      onPressed: () {
+                        Navigator.pop(context);
+                      },
+                      icon: Icon(Icons.arrow_back, color: Colors.white),
+                    ),
                   ),
-                ),
-              ],
-            ),
-
-            Expanded(
-              child: SingleChildScrollView(
-                child: Container(
-                  decoration: BoxDecoration(
-                    color: Colors.white,
-                    borderRadius: BorderRadius.all(Radius.circular(16)),
-                  ),
-                  child: Padding(
-                    padding: const EdgeInsets.all(8.0),
-                    child: Column(
-                      children: [
-                        Text(
-                          mainCardData.title,
-                          style: TextStyle(fontSize: 30, fontFamily: 'Somar'),
-                          textAlign: TextAlign.center,
-                        ),
-                        SizedBox(height: 10),
-                        Align(
-                          alignment: Alignment.topRight,
-                          child: Text(
-                            mainCardData.description,
-                            style: TextStyle(fontSize: 18, fontFamily: 'Somar'),
-                            textAlign: TextAlign.right,
+                ],
+              ),
+          
+              Positioned(
+                top: 260,
+                bottom: 0,
+                left: 0,
+                right: 0,
+                child: SingleChildScrollView(
+                  child: Container(
+                    decoration: BoxDecoration(
+                      color: Colors.white,
+                      borderRadius: BorderRadius.all(Radius.circular(30)),
+                    ),
+                    child: Padding(
+                      padding: const EdgeInsets.all(8.0),
+                      child: Column(
+                        children: [
+                          Text(
+                            mainCardData.title,
+                            style: TextStyle(
+                              fontSize: 30,
+                              fontFamily: 'Somar',
+                            ),
+                            textAlign: TextAlign.center,
                           ),
-                        ),
-                      ],
+                          SizedBox(height: 10),
+                          Align(
+                            alignment: Alignment.topRight,
+                            child: Text(
+                              mainCardData.description,
+                              style: TextStyle(
+                                fontSize: 18,
+                                fontFamily: 'Somar',
+                              ),
+                              textAlign: TextAlign.right,
+                            ),
+                          ),
+                        ],
+                      ),
                     ),
                   ),
                 ),
               ),
-            ),
-          ],
+            ],
+          ),
         ),
       ),
     );
