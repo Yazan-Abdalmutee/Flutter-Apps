03b64a91f1102e42abf89057259a986b0e7bcff4
yazan <yazanshrouf149@gmail.com>
2025-05-04 16:38:49 +0300

    initial commit

--- lib/main.dart
+++ lib/main.dart
@@ -3,16 +3,7 @@ import 'package:flutter/material.dart';
 import 'apps/fitness_magazine/views/magazine_home_page.dart';
 
 void main() {
-  runApp(const MyApp());
+  runApp(const FitnessPage());
 }
 
-class MyApp extends StatelessWidget {
-  const MyApp({super.key});
 
-  @override
-  Widget build(BuildContext context) {
-
-    return FitnessPage() ;
-
-  }
-}
