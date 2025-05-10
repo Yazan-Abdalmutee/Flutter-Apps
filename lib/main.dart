57656642438705009be09fc192c0d1e9ba61b0cf
yazan <yazanshrouf149@gmail.com>
2025-05-10 23:53:17 +0300

    initial commit

--- lib/main.dart
+++ lib/main.dart
@@ -4,5 +4,5 @@ import 'apps/fitness_magazine/fitness_magazine_app.dart';
 import 'apps/shipping_sample_app/pages/home_page.dart';
 
 void main() {
-  runApp(const FitnessApp());
+  runApp(const ShippingPage());
 }
